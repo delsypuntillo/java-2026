d1b0c878ac70f9d20bfbd459c805d1966978f1cf
delsy puntillo <delsypuntillo29@gmail.com>
2026-06-04 16:56:30 -0500

    clase y ejercico de bucles

---
+++ src/Bucles/TablaMultplicar.java
@@ -0,0 +1,14 @@
+package Bucles;
+
+public class TablaMultplicar {
+    public static void main(String[] args) {
+        int numero = 1;
+        while (numero <= 10){
+            System.out.println("5 x" + numero + "="+ (5 * numero));
+
+            numero++;
+
+
+        }
+    }
+}
---
+++ src/Bucles/mientras.java
@@ -0,0 +1,21 @@
+package Bucles;
+
+public class mientras {
+    public static void main(String[] args) {
+        int numero = 0;
+        while (numero <= 10){
+            System.out.println(numero);
+            numero = numero + 2;// forma extensa
+            numero +=2;// forma corta, usando operador de asisgacion
+
+
+        }
+
+
+
+
+
+
+
+    }
+}
